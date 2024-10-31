27e9d93c06ce60fa3779f3e1d8b0872f57b643b7
laurglenn <laurenglenn1988@gmail.com>
2024-10-30 18:32:08 -0700

    Update loanrepayments.py

--- loanrepayments.py
+++ loanrepayments.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
 
 st.title("Mortgage Repayments Calculator")
 
@@ -27,7 +30,34 @@ total_interest = total_payments - loan_amount
 
 st.write("### Repayments")
 col1, col2, col3 = st.columns(3)
-#Add metric to make the labels bolder
+#Add metric to make the values bolder
 col1.metric(label="Monthly Repayments", value=f"${monthly_payment:,.2f}")
 col2.metric(label="Total Repayments", value=f"${total_payments:,.0f}")
 col3.metric(label="Total Interest", value=f"${total_interest:,.0f}")
+
+# Create a data-frame with the payment schedule.
+schedule = []
+remaining_balance = loan_amount
+
+for i in range(1, number_of_payments + 1):
+    interest_payment = remaining_balance * monthly_interest_rate
+    principal_payment = monthly_payment - interest_payment
+    remaining_balance -= principal_payment
+    year = math.ceil(i / 12)  # Calculate the year into the loan
+    schedule.append(
+        [
+            i,
+            monthly_payment,
+            principal_payment,
+            interest_payment,
+            remaining_balance,
+            year,
+        ]
+    )
+
+df = pd.DataFrame(
+    schedule,
+    columns=["Month", "Payment", "Principal", "Interest", "Remaining Balance", "Year"],
+)
+
+
